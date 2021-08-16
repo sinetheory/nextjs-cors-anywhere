0c79b0678f258f895e9c9d8635be7827ab904adf
deybyr647 <deybyr647@gmail.com>
2021-08-16 00:06:39 -0400

    Added a landing page for testing APIs & modified backend logic

---
+++ components/Metadata.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+const Metadata = () => {
+    return (
+        <Head>
+            <meta charSet={"utf-8"}/>
+            <meta name={"viewport"} content={"width=device-width, initial-scale=1, shrink-to-fit=no"}/>
+            <meta name={"description"} content={"NextJS-CORS-Anywhere"}/>
+            <meta name={"keywords"} content={"NextJS, CORS, CORS-Anywhere, REST, API, JSON, REST API"}/>
+            <meta name={"author"} content={"Deyby Rodriguez"}/>
+            <meta name={"robots"} content={"index, follow"}/>
+
+            <meta property={"og:title"} content={"Deyby Rodriguez"}/>
+            <meta property={"og:type"} content={"website"}/>
+            <meta property={"og:image"} content={"https://deybyr647.com/opengraph.png"}/>
+            <meta property={"og:url"} content={"https://deybyr647.com"}/>
+            <meta property={"og:description"} content={"NextJS-CORS-Anywhere"}/>
+
+            <title>NextJS-CORS-Anywhere</title>
+        </Head>
+    )
+}
+
+export default Metadata;
